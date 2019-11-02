68f48fd1e2c9bda7530adc75078f74ff36806f53
Daniel Lucas de Souza Silva <dluc18@gmail.com>
2019-11-02 01:27:36 -0300

    add async button

--- lib/shared/components/buttons.dart
+++ lib/shared/components/buttons.dart
@@ -256,3 +256,77 @@ class FavoriteButton extends StatelessWidget {
     );
   }
 }
+
+
+class AsyncButton<T> extends StatefulWidget {
+  final Widget leading;
+  final Widget trailing;
+  final Widget child;
+  final EdgeInsets padding;
+  final Color color;
+  final Color textColor;
+  final Color iconColor;
+  final double iconSize;
+  final ShapeBorder shape;
+  final Clip clipBehavior;
+  final Future<T> Function(BuildContext context) builder;
+  final void Function(T) onSuccess;
+  final void Function(dynamic) onError;
+
+  const AsyncButton({
+    Key key,
+    this.leading,
+    this.trailing,
+    this.padding = kButtonPadding,
+    this.color,
+    this.textColor,
+    this.iconColor,
+    this.iconSize = 20,
+    this.shape,
+    this.clipBehavior = Clip.antiAlias,
+    this.child,
+    @required this.builder,
+    @required this.onSuccess,
+    @required this.onError,
+  }) : super(key: key);
+
+  @override
+  _AsyncButtonState createState() => _AsyncButtonState();
+}
+
+class _AsyncButtonState extends State<AsyncButton> {
+  bool _loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Button(
+      leading: widget.leading,
+      trailing: widget.trailing,
+      child: widget.child,
+      loading: _loading,
+      onPressed: () async {
+        setState(() {
+          _loading = true;
+        });
+        try {
+          await widget.builder(context).then(widget.onSuccess);
+        } catch (e) {
+          widget.onError(e);
+        } finally {
+          setState(() {
+            _loading = false;
+          });
+        }
+      },
+      padding: widget.padding,
+      color: widget.color ?? theme.buttonColor,
+      textColor: widget.textColor,
+      iconColor: widget.iconColor,
+      iconSize: widget.iconSize,
+      shape: widget.shape,
+      clipBehavior: widget.clipBehavior,
+    );
+  }
+}
